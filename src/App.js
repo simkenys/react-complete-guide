09bf3f7494caf9197a52065cd9276e67a7a81fd3
Simke <simke.nys@gmail.com>
2022-01-04 14:09:04 +0100

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2020, 7, 15),
     },
     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
